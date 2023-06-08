00e79ed367ff67588be5f1dac26d4532680287c6
Stiflerzak <hilarymwendia97@gmail.com>
2023-06-08 07:41:38 +0300

    initial

---
+++ src/main/java/io/hilary/coolcapital/CoolcapitalApplication.java
@@ -0,0 +1,13 @@
+package io.hilary.coolcapital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolcapitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoolcapitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/hilary/coolcapital/CoolcapitalApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hilary.coolcapital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolcapitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
